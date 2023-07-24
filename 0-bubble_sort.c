cfc9b7b12c110362652d8be2c91f6ff90f546fc3
FadyWefky <fady.wefky33@gmail.com>
2023-07-24 15:15:54 +0300

    edit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,46 @@
 #include "sort.h"
+
 /**
- * bubble_sort - function that sorts an array using bubble sort algorithm
- * @array: array to be sorted
- * @size: size of the array
- * Return: void
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	int isSwapped = 0;
-	size_t i;
-	size_t j;
-	/* Iterate through the array */
-	for (i = 0; i < size; i++)
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
 	{
-		/* Compare adjacent elements and swap if necessary */
-		for (j = 0; j < size - 1; j++)
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				/* Swap elements */
-				temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
-				/* Set flag to indicate a swap occurred */
-				isSwapped = 1;
-				print_array(array, size); /* Print the array after each swap */
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
-		/* If no swap occurred in the inner loop, the array is already sorted */
-		if (isSwapped == 0)
-		{
-			break;
-		}
+		len--;
 	}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,108 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "sort.h"
 
-typedef struct listint_t
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-    int n;
-    struct listint_t *prev;
-    struct listint_t *next;
-} listint_t;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
 
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2) {
-    listint_t *temp_prev;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
 
-    if (node1 == node2) return;
+	else
+		*h = n2;
 
-    temp_prev = node1->prev;
-    if (node1->prev != NULL)
-        node1->prev->next = node2;
-    if (node2->next != NULL)
-        node2->next->prev = node1;
-    node1->prev = node2->prev;
-    node2->prev = temp_prev;
-
-    temp_prev = node1->next;
-    node1->next = node2->next;
-    node2->next = temp_prev;
-
-    if (node1->prev == NULL)
-        *list = node1;
-    if (node2->prev == NULL)
-        *list = node2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
-void insertion_sort_list(listint_t **list) {
-    listint_t *current;
-    listint_t *key, *prev;
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *tmp;
 
-    if (*list == NULL || (*list)->next == NULL) return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current = (*list)->next;
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
 
-    while (current != NULL)
-    {
-        key = current;
-        prev = current->prev;
-
-        while (prev != NULL && key->n < prev->n)
-        {
-            swap_nodes(list, key, prev);
-            prev = key->prev;
-        }
-
-        // Print the list after each iteration
-        print_list(*list);
-
-        current = current->next;
-    }
-}
-
-listint_t *create_node(int n) {
-    listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-    if (new_node != NULL)
-    {
-        new_node->n = n;
-        new_node->prev = NULL;
-        new_node->next = NULL;
-    }
-    return new_node;
-}
-
-void append_node(listint_t **list, listint_t *new_node) {
-    listint_t *last_node;
-    
-    if (*list == NULL)
-    {
-        *list = new_node;
-    }
-    else
-    {
-        last_node = *list;
-        while (last_node->next != NULL)
-        {
-            last_node = last_node->next;
-        }
-        last_node->next = new_node;
-        new_node->prev = last_node;
-    }
-}
-
-void print_list(listint_t *list) {
-    while (list != NULL)
-    {
-        printf("%d ", list->n);
-        list = list->next;
-    }
-    printf("\n");
-}
-
-void free_list(listint_t **list) {
-    listint_t *current = *list;
-    while (current != NULL)
-    {
-        listint_t *next = current->next;
-        free(current);
-        current = next;
-    }
-    *list = NULL;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
